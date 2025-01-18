05fdfec8131e7eec6794febc9ef240d2a7c1b486
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-01-18 17:46:12 -0600

    Update .env.example, weatherRoutes, and server.ts

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+ import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', (_req: Request, res: Response) => {
+
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
